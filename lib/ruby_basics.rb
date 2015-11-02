648f6b9e2786d995d64ff5218593b695559f8d7a
intersim <emily.intersimone@gmail.com>
2015-11-02 00:04:16 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+  div = num1/num2
+  div
 end
 
 def assign_variable(value)
-  
+  var = value
+  var
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
-
+def greeting(greeting, name)
+  puts "#{greeting}#{name}!"
 end
 
 def return_a_value
-
+  a_value = "Nice"
+  a_value
 end
 
 def last_evaluated_value
-
+  value = "expert"
+  value
 end
 
-def pizza_party
-
+def pizza_party(pizza="cheese")
+  puts "I would like a #{pizza} pizza, please."
+  pizza
 end
